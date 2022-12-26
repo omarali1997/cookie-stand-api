8abfcae12c48626d4669e4f0a5ce7abd4aa62cb9
omarali1997 <omar.aziz.9718@gmail.com>
2022-12-26 22:59:51 +0000

    ff

--- cookie_stands/models.py
+++ cookie_stands/models.py
@@ -3,14 +3,15 @@ from django.db import models
 
 
 class CookieStand(models.Model):
-    
-    location = models.CharField(max_length=256)
-    owner = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=256)
+    owner = models.ForeignKey(
+        get_user_model(), on_delete=models.CASCADE, null=True, blank=True
+    )
     description = models.TextField(default="", null=True, blank=True)
     hourly_sales = models.JSONField(default=list, blank=True)
     minimum_customers_per_hour = models.IntegerField(default=0)
     maximum_customers_per_hour = models.IntegerField(default=0)
     average_cookies_per_sale = models.FloatField(default=0)
-    
+
     def __str__(self):
         return self.name
